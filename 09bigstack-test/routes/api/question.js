90aac6e8fa7e263fab5ea57fc023c262f32547d9
amitsingh1234567 <50829353+amitsingh1234567@users.noreply.github.com>
2019-05-20 21:26:54 +0530

    Delete question.js

--- 09bigstack-test/routes/api/question.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.json({question:'question is success'});
-});
-
-module.exports = router;
