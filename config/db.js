1f42e1897fa6d09c516569bd13826d92e98acf80
amitsingh1234567 <50829353+amitsingh1234567@users.noreply.github.com>
2020-06-18 07:53:28 +0530

    Delete db.js

--- config/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const key = 'mongodb+srv://amit-singh_1:9576435668@cluster0-49vnb.mongodb.net/test?retryWrites=true'
-mongoose.connect(key, { useUnifiedTopology: true, useNewUrlParser: true  })
-.then(() => console.log('MongoDb Connected Successfully..'))
-.catch(err => console.log(err));
